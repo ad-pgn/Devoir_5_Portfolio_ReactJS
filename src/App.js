cbc12d594314fb6c56888f0d64461150ad638ef1
AdrianP <adrian.pogneau@gmail.com>
2026-05-26 13:50:16 +0200

    close #2 - Composant Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+
 function App() {
   return (
-    <div>
-      <h1>Portfolio John Doe</h1>
-    </div>
-  );
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<div>Acceuil</div>}/>
+        <Route path='/services' element={<div>Services</div>}/>
+        <Route path='/portfolio' element={<div>Portfolio</div>}/>
+        <Route path='/contact' element={<div>Contact</div>}/>
+        <Route path='/mentions-legales' element={<div>Mentions légales</div>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function Header() {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+            <Container fluid="lg">
+                <Navbar.Brand as={NavLink} to="/" className="fw-semibold">
+                    JOHN DOE
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="main-nav"/>
+                <Navbar.Collapse id="main-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={NavLink} to="/" end className="text-uppercase mx-1">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/services" end className="text-uppercase mx-1">
+                            Services
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/Portfolio" end className="text-uppercase mx-1">
+                            Portfolio
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/contact" end className="text-uppercase mx-1">
+                            Contact
+                        </Nav.Link>
+                        <Nav.Link as={NavLink} to="/mentions-legales" end className="text-uppercase mx-1">
+                            Mention légales
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header;
